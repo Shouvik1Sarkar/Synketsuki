b9e74a4ee21d5281d8282e98257eef4fb48567be
Shouvik1Sarkar <shouvik3501@gmail.com>
2026-08-02 18:01:26 +0530

    Add user role management functionality; implement role change and user deletion endpoints

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -2,6 +2,7 @@ import User from "../models/user.models.js";
 import ApiError from "../utils/ApiError.utils.js";
 import ApiResponse from "../utils/ApiResponse.utils.js";
 import asyncHandler from "../utils/asyncHandlers.utils.js";
+import { available_roles_enum } from "../utils/constants.utils.js";
 
 // export const Search_users = asyncHandler(async (req, res) => {});
 
@@ -29,5 +30,39 @@ export const Suspend_unsuspend_users = asyncHandler(async (req, res) => {
 
   return res.status(200).json(new ApiResponse(200, find_user, message));
 });
-export const Delete_users = asyncHandler(async (req, res) => {});
-export const Change_user_roles = asyncHandler(async (req, res) => {});
+export const delete_users = asyncHandler(async (req, res) => {
+  const user = req.user;
+
+  const { id } = req.params;
+
+  await User.findByIdAndDelete(id);
+
+  return res.status(200).json(new ApiResponse(200, null, "user deleted"));
+});
+export const change_user_roles = asyncHandler(async (req, res) => {
+  const user = req.user;
+
+  const { id } = req.params;
+  const { user_role } = req.body;
+
+  if (!available_roles_enum.includes(user_role)) {
+    throw new ApiError(400, "User roled not available.");
+  }
+  if (!id) throw new ApiError(400, "Id is required.");
+
+  const find_user = await User.findById(id).select("-password");
+
+  if (!find_user) throw new ApiError(400, "User not found");
+
+  if (find_user.role === user_role) {
+    throw new ApiError(400, "User already has this role.");
+  }
+
+  find_user.role = user_role;
+
+  await find_user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, find_user, "user role updated"));
+});
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -6,15 +6,31 @@ import { registerValidator } from "../utils/auth_validate.utils.js";
 
 import { authorize } from "../middlewares/authorize_roles.middlewares.js";
 import { available_user_roles } from "../utils/constants.utils.js";
-import { Suspend_unsuspend_users } from "../controllers/admin.controllers.js";
+import {
+  change_user_roles,
+  delete_users,
+  Suspend_unsuspend_users,
+} from "../controllers/admin.controllers.js";
 
 const adminRouter = express.Router();
 
-adminRouter.get(
+adminRouter.patch(
   "/suspension/:id",
   logInAuth,
   authorize("user", "product_owner", "product_admin"),
   Suspend_unsuspend_users,
+);
+adminRouter.delete(
+  "/delete/:id",
+  logInAuth,
+  authorize("user", "product_owner", "product_admin"),
+  delete_users,
+);
+adminRouter.patch(
+  "/change-user-roles/:id",
+  logInAuth,
+  authorize("user", "product_owner", "product_admin"),
+  change_user_roles,
 );
 
 export default adminRouter;
